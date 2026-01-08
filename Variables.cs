f062b1f2199923752a40f89c73880346bc8d9ed6
alirezajahanshahii1390-ship-it <alirezajahanshahii1390@gmail.com>
2026-01-08 10:37:54 +0330

    Add variables example

---
+++ Variables.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Variables
+{
+    static void Main()
+    {
+        int age = 13;
+        string name = "Ali";
+        Console.WriteLine(name + " is " + age + " years old");
+    }
+}
